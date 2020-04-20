288967d81538549985a14bba2c05dd506b6cb66d
Maxwell Dubin <maxdhs@gmail.com>
2020-04-20 15:53:36 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function App() {
+  return (
+    <Text style={{ marginTop: 100 }}>Hello Nucamp React Native Class!</Text>
+  );
+}
